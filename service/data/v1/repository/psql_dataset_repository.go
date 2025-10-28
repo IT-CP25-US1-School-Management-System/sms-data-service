ad45250a706d913f6084db89fa314c6f4e21ba69
TheGode <phuttinan.phak@kmutt.ac.th>
2025-10-29 02:45:45 +0700

    🐌 feat(api): Add SortOrder parameter for dataset queries

--- service/data/v1/repository/psql_dataset_repository.go
+++ service/data/v1/repository/psql_dataset_repository.go
@@ -21,126 +21,124 @@ type psqlDatasetRepository struct {
 
 // FetchDatasetList implements data.PsqlDatasetRepository.
 func (p *psqlDatasetRepository) FetchDatasetList(ctx context.Context, filter *filter.DatasetsFilter, paginator *helperModel.Paginator) ([]*entity.Datasets, error) {
-	var where string
-	var conds = make([]string, 0)
-	var valArgs = make([]interface{}, 0)
-	var orderBy string
-	var paginatorSql string
+	var (
+		conds    []string
+		valArgs  []interface{}
+		orderBy  string
+		where    string
+		limitSQL string
+	)
 
 	if filter != nil {
 		if filter.Domain != "" {
-			conds = append(conds, "domain=?")
+			conds = append(conds, "domain = ?")
 			valArgs = append(valArgs, filter.Domain)
 		}
 		if filter.Owner != "" {
-			conds = append(conds, "owner=?")
+			conds = append(conds, "owner = ?")
 			valArgs = append(valArgs, filter.Owner)
 		}
 		if filter.SearchWord != "" {
-			filter.SearchWord = fmt.Sprintf("%%%s%%", strings.ToLower(strings.ReplaceAll(filter.SearchWord, " ", "")))
-			conds = append(conds, "(LOWER(REPLACE(name, ' ', '')) LIKE ? OR LOWER(REPLACE(description, ' ', '')) LIKE ?)")
-			valArgs = append(valArgs, filter.SearchWord, filter.SearchWord)
+			sw := fmt.Sprintf("%%%s%%", strings.ToLower(strings.ReplaceAll(filter.SearchWord, " ", "")))
+			conds = append(conds, "(LOWER(REPLACE(name,' ','')) LIKE ? OR LOWER(REPLACE(description,' ','')) LIKE ?)")
+			valArgs = append(valArgs, sw, sw)
 		}
 		if len(filter.Tags) > 0 {
 			conds = append(conds, "COALESCE(tags, '{}'::text[]) && ?::text[]")
 			valArgs = append(valArgs, pq.Array(filter.Tags))
 		}
 		if filter.HasPii != nil {
-			conds = append(conds, "has_pii=?")
+			conds = append(conds, "has_pii = ?")
 			valArgs = append(valArgs, *filter.HasPii)
 		}
-		if filter.SortBy != "" && filter.SortOrder != "" {
-			if filter.SortOrder == constants.SORT_ORDER_ASC || filter.SortOrder == constants.SORT_ORDER_DESC {
-				if filter.SortBy == constants.DATASET_SORT_BY_NAME || filter.SortBy == constants.DATASET_SORT_BY_CREATED_AT || filter.SortBy == constants.DATASET_SORT_BY_UPDATED_AT {
-					orderBy = fmt.Sprintf("%s %s", filter.SortBy, filter.SortOrder)
-				}
+
+		validSortColumns := map[string]bool{
+			constants.DATASET_SORT_BY_NAME:       true,
+			constants.DATASET_SORT_BY_CREATED_AT: true,
+			constants.DATASET_SORT_BY_UPDATED_AT: true,
+		}
+		validSortOrders := map[string]bool{
+			constants.SORT_ORDER_ASC:  true,
+			constants.SORT_ORDER_DESC: true,
+		}
+		if filter.SortBy != "" && validSortColumns[filter.SortBy] {
+			order := constants.SORT_ORDER_DESC
+			if validSortOrders[filter.SortOrder] {
+				order = filter.SortOrder
 			}
-		} else {
-			orderBy = "created_at DESC"
+			orderBy = fmt.Sprintf("ORDER BY %s %s", filter.SortBy, order)
 		}
 	}
+
 	if len(conds) > 0 {
 		where = "WHERE " + strings.Join(conds, " AND ")
 	}
-	if orderBy != "" {
-		orderBy = "ORDER BY " + orderBy
+	if orderBy == "" {
+		orderBy = "ORDER BY created_at DESC"
 	}
-
 	if paginator != nil {
-		var limit = paginator.GetLimit()
-		var offSet = paginator.GetOffset()
-		paginatorSql = fmt.Sprintf(`
-			LIMIT %d
-			OFFSET %d
-			`,
-			limit,
-			offSet,
-		)
+		limitSQL = `
+			LIMIT ?
+			OFFSET ?
+		`
+		valArgs = append(valArgs, paginator.GetLimit(), paginator.GetOffset())
 	}
 
-	query := fmt.Sprintf(`
-    SELECT 
-		id, 
-		name, 
-	    domain, 
-	    owner, 
-	    sensitivity, 
-	    has_pii, 
-	    tags, 
-	    description, 
-	    created_at, 
-	    updated_at, 
-	    COUNT(*) OVER() as total_row 
-	FROM 
-	datasets 
-	%s 
-	%s 
-	%s
-	`,
-		where,
-		orderBy,
-		paginatorSql,
-	)
+	dataSQL := fmt.Sprintf(`
+		SELECT
+			id, name, domain, owner, sensitivity, has_pii, tags, description, created_at, updated_at
+		FROM datasets
+		%s
+		%s
+		%s
+	`, where, orderBy, limitSQL)
+	dataSQL = sqlx.Rebind(sqlx.DOLLAR, dataSQL)
 
-	query = sqlx.Rebind(sqlx.DOLLAR, query)
-
-	stmt, err := p.client.GetClient().PreparexContext(ctx, query)
-	if err != nil {
-		return nil, err
-	}
-	defer stmt.Close()
-
-	rows, err := stmt.QueryxContext(ctx, valArgs...)
+	rows, err := p.client.GetClient().QueryxContext(ctx, dataSQL, valArgs...)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var totalRow int
 	var datasets []*entity.Datasets
 	for rows.Next() {
-		var dataset entity.Datasets
+		var d entity.Datasets
 		if err := rows.Scan(
-			&dataset.ID,
-			&dataset.Name,
-			&dataset.Domain,
-			&dataset.Owner,
-			&dataset.Sensitivity,
-			&dataset.HasPii,
-			pq.Array(&dataset.Tags),
-			&dataset.Description,
-			&dataset.CreatedAt,
-			&dataset.UpdatedAt,
-			&totalRow,
+			&d.ID,
+			&d.Name,
+			&d.Domain,
+			&d.Owner,
+			&d.Sensitivity,
+			&d.HasPii,
+			pq.Array(&d.Tags),
+			&d.Description,
+			&d.CreatedAt,
+			&d.UpdatedAt,
 		); err != nil {
 			return nil, err
 		}
-		datasets = append(datasets, &dataset)
+		datasets = append(datasets, &d)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
 	}
 
-	if paginator != nil && len(datasets) > 0 {
-		paginator.SetPaginatorByAllRows(totalRow)
+	var totalRows int
+	countArgs := append([]interface{}(nil), valArgs...)
+	if paginator != nil {
+		countArgs = countArgs[:len(countArgs)-2]
 	}
+	countSQL := fmt.Sprintf(`SELECT COUNT(*) FROM datasets %s`, where)
+	countSQL = sqlx.Rebind(sqlx.DOLLAR, countSQL)
+
+	if err := p.client.GetClient().GetContext(ctx, &totalRows, countSQL, countArgs...); err != nil {
+		return nil, err
+	}
+
+	if paginator != nil {
+		paginator.SetPaginatorByAllRows(totalRows)
+	}
+
 	return datasets, nil
 }
 
